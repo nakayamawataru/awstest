c6cf0b6b95affd93af6c2f6f2c4bc6c5e7120c33
nakayamawataru <nakayama.wataru.GP01@gmail.com>
2020-01-07 22:00:59 +0900

    rspec修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
+  before do
+    @post = build(:post)
+  end
+
+  it "タイトルがなければNG" do
+    @post = build(:post, title: "")
+    expect(@post.valid?).to eq(false)
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
